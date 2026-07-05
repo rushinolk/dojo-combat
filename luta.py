786417b3d8f7d364ea7946140088ef8dba7d369c
rushinolk <arthurmachado7191@hotmail.com>
2026-07-05 18:24:42 -0300

    refactor: Logica fora do loop

--- luta.py
+++ luta.py
@@ -1,21 +1,53 @@
 import random
 
 
-def receber_dano(atacante,defensor):
-    defensor["vida"] -= atacante["atk"]
-
 def rolar_dado():
     return random.random()
 
+def receber_dano(atacante,defensor):
+    defensor["vida"] -= atacante["atk"]
+
+def realizar_ataque(player,npc):
+    receber_dano(player,npc)
+    receber_dano(npc,player)
+
+
+def defender(player,npc):
+    chance = rolar_dado() 
+
+    if chance > 0.8:
+        print("Esquivou com sucesso!")
+
+    elif chance < 0.4:
+        atk_original = npc["atk"]
+        npc["atk"] = npc["atk"] / random.randint(2,4)
+        receber_dano(npc,player)
+        npc["atk"] = atk_original
+
+
+    else:
+        print("Defesa falhou!")
+        receber_dano(npc, player)
+
+
 def is_endgame(player, npc):
-    if player["vida"] <= 0 or npc["vida"] <= 0:
-        return True
+    if player["vida"] <= 0 or npc["vida"] <= 0:       
+        if player["vida"] <= 0:
+            print("Você perdeu!")
+            return True
+        else:
+            print("Você venceu!")
+            return True
+    return False    
+
+
+
 
 
 
 if __name__ == "__main__":
 
-    end_game = 0
+    end_game = False
     turno = 1
 
     player = {
@@ -26,22 +58,20 @@ if __name__ == "__main__":
 
     nomes = ["killik","bowden","RUIM"]
 
-
     npc = {
-        "nome": random.choice(nomes),
-        "vida": random.randint(150,200),
-        "atk": random.randint(10,30) 
-    }
-
-    while end_game != 1:
-        
-        if turno == 1:
-            npc = {
                 "nome": random.choice(nomes),
                 "vida": random.randint(150,200),
                 "atk": random.randint(10,30) 
-            }
+    }
+ 
+    while end_game != True:
+        
+            
 
+        move_set = {
+            "1":realizar_ataque,
+            "2":defender
+        }
 
         print(f"Turno: {turno}")
 
@@ -54,36 +84,18 @@ if __name__ == "__main__":
 
 
 
-
+        # PESQUISAR TRY/EXCEPT PARA INPUT
+        # COMO UTILIZAR O DICIONARIO DE FUNÇÔES
         escolha = int(input("\n\nEscolha uma opção: \n1 - Atacar \n2 - Defender\n"))
 
-        if escolha not in [1, 2]:
+        if escolha not in move_set:
             print("Escolha inválida")
             continue
-
-
-        if escolha == 1:
-            receber_dano(player,npc)
-            receber_dano(npc,player)
-
-        if escolha == 2:
-            chance = rolar_dado() 
+         
+            
         
-            if chance > 0.8:
-                print("Esquivou com sucesso!")
-            else:
-                print("Defesa falhou!")
-                receber_dano(npc, player)
-
-
-        if is_endgame(player,npc):
-            end_game = 1
-            if player["vida"] <= 0:
-                print("Você perdeu!")
-            else:
-                print("Você venceu!")
-
-
+        
+        end_game = is_endgame(player,npc)
         turno += 1
         
 
