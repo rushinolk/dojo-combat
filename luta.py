992609223e2b2eafdc97df4ab02ed7aa1fcbd41f
rushinolk <arthurmachado7191@hotmail.com>
2026-07-06 17:59:06 -0300

    refactor: Conclusão dict de funcoes, add: engine_db

--- luta.py
+++ luta.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 
 def rolar_dado():
@@ -7,27 +8,57 @@ def rolar_dado():
 def receber_dano(atacante,defensor):
     defensor["vida"] -= atacante["atk"]
 
-def realizar_ataque(player,npc):
-    receber_dano(player,npc)
-    receber_dano(npc,player)
+def realizar_ataque(atacante,defensor):
+    print(f"{atacante.get('nome')} atacou {defensor.get('nome')}")
+    time.sleep(0.6)
+    receber_dano(atacante,defensor)
+    print(f"{defensor.get('nome')} recebeu {atacante.get('atk')} de dano!")
+    time.sleep(0.6)
 
+    if defensor["vida"] > 0:
+        print(f"{defensor.get('nome')} atacou {atacante.get('nome')}")
+        time.sleep(0.6)
+        receber_dano(defensor,atacante)
+        print(f"{atacante.get('nome')} recebeu {defensor.get('atk')} de dano!")       
+        time.sleep(0.6)
 
 def defender(player,npc):
     chance = rolar_dado() 
 
     if chance > 0.8:
+        time.sleep(0.6)
         print("Esquivou com sucesso!")
 
     elif chance < 0.4:
         atk_original = npc["atk"]
         npc["atk"] = npc["atk"] / random.randint(2,4)
         receber_dano(npc,player)
+        time.sleep(0.6)
+        print("Defesa parcial! Você recebeu dano reduzido!")
         npc["atk"] = atk_original
 
 
     else:
+        time.sleep(1)
         print("Defesa falhou!")
         receber_dano(npc, player)
+
+
+def use_special(player, npc):
+    if player["cooldown_special"] == 0:
+        print(f"{player.get('nome')} usou o ataque especial!")
+        time.sleep(0.6)
+        player["atk"] = player["atk"] * 2
+        receber_dano(player, npc)
+        print(f"{npc.get('nome')} recebeu {player.get('atk')} de dano!")
+
+        # CAUSAR SANGRAMENTO
+
+        time.sleep(0.6)
+        player["atk"] = player["atk"] / 2
+        player["cooldown_special"] = 3
+    else:
+        print(f"Ataque especial em cooldown! Aguarde {player['cooldown_special']}  turnos.")
 
 
 def is_endgame(player, npc):
@@ -43,6 +74,10 @@ def is_endgame(player, npc):
 
 
 
+def bleeding(atacante,defensor):
+    pass
+
+
 
 
 if __name__ == "__main__":
@@ -53,7 +88,8 @@ if __name__ == "__main__":
     player = {
         "nome": "Leywin",
         "vida": 200,
-        "atk": 25
+        "atk": 25,
+        "cooldown_special": 0
     }
 
     nomes = ["killik","bowden","RUIM"]
@@ -63,36 +99,43 @@ if __name__ == "__main__":
                 "vida": random.randint(150,200),
                 "atk": random.randint(10,30) 
     }
+
+    move_set = {
+            "1":realizar_ataque,
+            "2":defender,
+            "3":use_special
+    }
  
     while end_game != True:
-        
-            
 
-        move_set = {
-            "1":realizar_ataque,
-            "2":defender
-        }
-
-        print(f"Turno: {turno}")
+        time.sleep(1.5)
+        print(f"\n\nTurno: {turno}")
 
         print(f"{player.get("nome")}")
         print(f"Vida: {player.get("vida")}")
 
         print("------------------------------")
+
         print(f"{npc.get("nome")} NPC Boladão")
-        print(f"Vida: {npc.get("vida")}")
+        print(f"Vida: {npc.get("vida")}\n")
 
 
 
-        # PESQUISAR TRY/EXCEPT PARA INPUT
-        # COMO UTILIZAR O DICIONARIO DE FUNÇÔES
-        escolha = int(input("\n\nEscolha uma opção: \n1 - Atacar \n2 - Defender\n"))
+        
+        try:
+            escolha = input("\n\nEscolha uma opção: \n1 - Atacar \n2 - Defender\n3 - Ataque Especial\n\n")
 
-        if escolha not in move_set:
-            print("Escolha inválida")
-            continue
-         
-            
+        except ValueError:
+            print("Entrada inválida!")
+
+        if escolha == "0":
+            print("Encerrando programa...")
+            break
+
+        if escolha in move_set:
+            funcao_escolhida = move_set[escolha]
+            funcao_escolhida(player,npc)
+
         
         
         end_game = is_endgame(player,npc)
