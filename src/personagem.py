82108ed7f019c17fa8fe80cacd1d724c942c6b02
rushinolk <arthurmachado7191@hotmail.com>
2026-08-16 14:46:17 -0300

    feat: class personagem com seus atributos base

---
+++ src/personagem.py
@@ -0,0 +1,12 @@
+
+
+
+class Personagem:
+    def __init__(self, nome, energia, atk, defesa, agilidade, concentracao, percepcao):
+        self.nome = nome
+        self.energia = energia
+        self.atk = atk
+        self.defesa = defesa
+        self.agilidade = agilidade
+        self.concentracao = concentracao
+        self.percepcao = percepcao
